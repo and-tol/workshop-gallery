55091d70ea43e059eeec9e8d8faae3305ae4d18b
Andrey Prisniak <aprisniak@lectrum.io>
2020-07-08 20:26:27 +0300

    1

---
+++ src/Gallery/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './styles/init.css';
+
+export const Gallery = () => {
+    return (
+        <>
+            <h1 className = 'title'>Галерея</h1>
+            <div className = 'gallery-wrapper'>
+                <div className = 'gallery'>
+                    <div>IMG</div>
+                </div>
+            </div>
+        </>
+    );
+};
--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Проект готов к началу воркшопа!</h1>, document.getElementById('root'));
+import { Gallery } from './Gallery';
+
+ReactDOM.render(<Gallery />, document.getElementById('root'));
