4971b1c00e2d407ffbc9ccfd631b85912260bbd9
Andrey Prisniak <aprisniak@lectrum.io>
2020-07-08 20:53:13 +0300

    4

--- src/Gallery/index.js
+++ src/Gallery/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './styles/init.css';
 
@@ -6,6 +6,8 @@ import './styles/init.css';
 import images from './assets/data/images.json';
 
 export const Gallery = () => {
+    const [ imageId, setImageId ] = useState(images[ 0 ].id);
+
     const createImagesJSX = () => {
         const result = images.map((item) => {
             const { id, src } = item;
@@ -22,12 +24,30 @@ export const Gallery = () => {
         return result;
     };
 
+    const createImageJSX = () => {
+        const result = images.find(({ id }) => id === imageId);
+        const { id, author, src, public: price } = result;
+
+        return (
+            <>
+                <div className = 'overlay'>
+                    <ul>
+                        <li>Идентификатор: {id}</li>
+                        <li>Автор: {author}</li>
+                        <li>Условия: {price ? 'Бесплатно' : '$10'}</li>
+                    </ul>
+                </div>
+                <img src = { src } />
+            </>
+        );
+    };
+
     return (
         <>
             <h1 className = 'title'>Галерея</h1>
             <div className = 'gallery-wrapper'>
                 <div className = 'gallery'>
-                    <div>IMG</div>
+                    <div>{ createImageJSX() }</div>
                 </div>
                 <div className = 'buttons-wrapper'>
                     <div className = 'buttons' >
